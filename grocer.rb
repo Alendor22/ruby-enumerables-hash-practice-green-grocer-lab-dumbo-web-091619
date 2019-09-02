9c6867c778f0c8f9ca51ec3faea975769666352d
Adeja Lendor <alendor6@gmail.com>
2019-09-02 22:33:04 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -29,6 +29,7 @@ def apply_coupons(cart, coupons)
       new_cart[c_name][:count] -= c_num
       if new_cart.has_key?("#{c_name} W/COUPON")
         new_cart["#{c_name} W/COUPON"][:count] += coupon[:num]
+      else
 end
 
 def apply_clearance(cart)
